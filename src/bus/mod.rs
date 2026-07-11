0ab5d9bcd66198041c5634b5e1d3b9058bb5ec94
tuxnode <tuxnode@outlook.com>
2026-07-11 17:16:21 +0800

    feat: implement simple bus for debug

--- src/bus/mod.rs
+++ src/bus/mod.rs
@@ -2,3 +2,5 @@ pub trait Bus {
     fn read(&mut self, addr: u16) -> u8;
     fn write(&mut self, addr: u16, val: u8);
 }
+
+pub mod simple;
---
+++ src/bus/simple.rs
@@ -0,0 +1,30 @@
+use crate::bus::Bus;
+
+pub struct SimpleBus {
+    memory: [u8; 0x10000],
+}
+
+impl SimpleBus {
+    pub fn new() -> Self {
+        Self {
+            memory: [0; 0x10000],
+        }
+    }
+
+    pub fn load(&mut self, data: &[u8], start: u16) {
+        for (i, byte) in data.iter().enumerate() {
+            let addr = start.wrapping_add(i as u16);
+            self.memory[addr as usize] = *byte;
+        }
+    }
+}
+
+impl Bus for SimpleBus {
+    fn read(&mut self, addr: u16) -> u8 {
+        self.memory[addr as usize]
+    }
+
+    fn write(&mut self, addr: u16, val: u8) {
+        self.memory[addr as usize] = val;
+    }
+}
