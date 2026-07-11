2a0e0c3157322afd3bd7d8e89f8d5474b82daf7c
tuxnode <tuxnode@outlook.com>
2026-07-11 12:20:04 +0800

    feat: Add Develop document information on Source Code

---
+++ src/cpu.rs
@@ -0,0 +1,5 @@
+// mos 6502 CPU mod
+
+// CMOS(65C02) Documents: 
+// - W65C02S datasheet：https://www.westerndesigncenter.com/wdc/documentation/w65c02s.pdf
+// - W65C02S Programming Manual：https://www.westerndesigncenter.com/wdc/documentation/w65c02-programming-manual.pdf
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
-}
+/*
+* MOS 6502 -> CMOS 5C02 Emulater
+* Integration Testing Kit: https://github.com/Klaus2m5/6502_65C02_functional_tests
+*/
+
+mod cpu;
+
+fn main() {}
