c8030220db1eec4261a95835262735ab5f09b276
tuxnode <tuxnode@outlook.com>
2026-07-12 16:12:28 +0800

    feat: implement Load/Store instructions
    - LDA: 9 addressing modes (IMM/ZP/ZPX/ABS/ABSX/ABSY/ZPI/ZPXI/AIY)
    - LDX: 5 addressing modes (IMM/ZP/ZPY/ABS/ABSY)
    - LDY: 5 addressing modes (IMM/ZP/ZPX/ABS/ABSX)
    - STA: 8 addressing modes (ZP/ZPX/ABS/ABSX/ABSY/ZPI/ZPXI/AIY)
    - STX: 3 addressing modes (ZP/ZPY/ABS)
    - STY: 3 addressing modes (ZP/ZPX/ABS)
    - STZ: 4 addressing modes (ZP/ZPX/ABS/ABSX)

--- src/instructions.rs
+++ src/instructions.rs
@@ -29,74 +29,69 @@ impl<B: Bus> Cpu<B> {
         match opcode {
             opcodes::NOP => 2,
 
-            //Stack
-            opcodes::PHA => {
-                self.push(self.a);
-                3
-            }
-            opcodes::PHP => {
-                self.push(self.status);
-                3
-            }
-            opcodes::PHX => {
-                self.push(self.x);
-                3
-            }
-            opcodes::PHY => {
-                self.push(self.y);
-                3
-            }
-
-            opcodes::PLA => {
-                self.a = self.pop();
-                self.update_nz(self.a);
-                4
-            }
-            opcodes::PLP => {
-                self.status = self.pop();
-                4
-            }
-            opcodes::PLX => {
-                self.x = self.pop();
-                self.update_nz(self.x);
-                4
-            }
-            opcodes::PLY => {
-                self.y = self.pop();
-                self.update_nz(self.y);
-                4
-            }
+            // Stack
+            opcodes::PHA => { self.push(self.a); 3 }
+            opcodes::PHP => { self.push(self.status); 3 }
+            opcodes::PHX => { self.push(self.x); 3 }
+            opcodes::PHY => { self.push(self.y); 3 }
+            opcodes::PLA => { self.a = self.pop(); self.update_nz(self.a); 4 }
+            opcodes::PLP => { self.status = self.pop(); 4 }
+            opcodes::PLX => { self.x = self.pop(); self.update_nz(self.x); 4 }
+            opcodes::PLY => { self.y = self.pop(); self.update_nz(self.y); 4 }
 
             // Transfer
-            opcodes::TAX => {
-                self.x = self.a;
-                self.update_nz(self.x);
-                2
-            }
-            opcodes::TAY => {
-                self.y = self.a;
-                self.update_nz(self.y);
-                2
-            }
-            opcodes::TXA => {
-                self.a = self.x;
-                self.update_nz(self.a);
-                2
-            }
-            opcodes::TYA => {
-                self.a = self.y;
-                self.update_nz(self.a);
-                2
-            }
-            opcodes::TSX => {
-                self.x = self.sp;
-                self.update_nz(self.x);
-                2
-            }
-            opcodes::TXS => {
-                self.sp = self.x;
-                2
-            }
+            opcodes::TAX => { self.x = self.a; self.update_nz(self.x); 2 }
+            opcodes::TAY => { self.y = self.a; self.update_nz(self.y); 2 }
+            opcodes::TXA => { self.a = self.x; self.update_nz(self.a); 2 }
+            opcodes::TYA => { self.a = self.y; self.update_nz(self.a); 2 }
+            opcodes::TSX => { self.x = self.sp; self.update_nz(self.x); 2 }
+            opcodes::TXS => { self.sp = self.x; 2 }
+
+            // Load
+            opcodes::LDA_IMM => { let a = self.immediate(); self.lda(a); 2 }
+            opcodes::LDA_ZP => { let a = self.zeropage(); self.lda(a); 3 }
+            opcodes::LDA_ZPX => { let a = self.zeropage_x(); self.lda(a); 4 }
+            opcodes::LDA_ABS => { let a = self.absolute(); self.lda(a); 4 }
+            opcodes::LDA_ABSX => { let a = self.absolute_x(); self.lda(a); 4 }
+            opcodes::LDA_ABSY => { let a = self.absolute_y(); self.lda(a); 4 }
+            opcodes::LDA_ZPI => { let a = self.pre_indexed_y(); self.lda(a); 5 }
+            opcodes::LDA_ZPXI => { let a = self.pre_indexed_x(); self.lda(a); 6 }
+            opcodes::LDA_AIY => { let a = self.post_indexed_y(); self.lda(a); 5 }
+
+            opcodes::LDX_IMM => { let a = self.immediate(); self.ldx(a); 2 }
+            opcodes::LDX_ZP => { let a = self.zeropage(); self.ldx(a); 3 }
+            opcodes::LDX_ZPY => { let a = self.zeropage_y(); self.ldx(a); 4 }
+            opcodes::LDX_ABS => { let a = self.absolute(); self.ldx(a); 4 }
+            opcodes::LDX_ABSY => { let a = self.absolute_y(); self.ldx(a); 4 }
+
+            opcodes::LDY_IMM => { let a = self.immediate(); self.ldy(a); 2 }
+            opcodes::LDY_ZP => { let a = self.zeropage(); self.ldy(a); 3 }
+            opcodes::LDY_ZPX => { let a = self.zeropage_x(); self.ldy(a); 4 }
+            opcodes::LDY_ABS => { let a = self.absolute(); self.ldy(a); 4 }
+            opcodes::LDY_ABSX => { let a = self.absolute_x(); self.ldy(a); 4 }
+
+            // Store
+            opcodes::STA_ZP => { let a = self.zeropage(); self.sta(a); 3 }
+            opcodes::STA_ZPX => { let a = self.zeropage_x(); self.sta(a); 4 }
+            opcodes::STA_ABS => { let a = self.absolute(); self.sta(a); 4 }
+            opcodes::STA_ABSX => { let a = self.absolute_x(); self.sta(a); 5 }
+            opcodes::STA_ABSY => { let a = self.absolute_y(); self.sta(a); 5 }
+            opcodes::STA_ZPI => { let a = self.pre_indexed_y(); self.sta(a); 5 }
+            opcodes::STA_ZPXI => { let a = self.pre_indexed_x(); self.sta(a); 6 }
+            opcodes::STA_AIY => { let a = self.post_indexed_y(); self.sta(a); 6 }
+
+            opcodes::STX_ZP => { let a = self.zeropage(); self.stx(a); 3 }
+            opcodes::STX_ZPY => { let a = self.zeropage_y(); self.stx(a); 4 }
+            opcodes::STX_ABS => { let a = self.absolute(); self.stx(a); 4 }
+
+            opcodes::STY_ZP => { let a = self.zeropage(); self.sty(a); 3 }
+            opcodes::STY_ZPX => { let a = self.zeropage_x(); self.sty(a); 4 }
+            opcodes::STY_ABS => { let a = self.absolute(); self.sty(a); 4 }
+
+            opcodes::STZ_ZP => { let a = self.zeropage(); self.stz(a); 3 }
+            opcodes::STZ_ZPX => { let a = self.zeropage_x(); self.stz(a); 4 }
+            opcodes::STZ_ABS => { let a = self.absolute(); self.stz(a); 4 }
+            opcodes::STZ_ABSX => { let a = self.absolute_x(); self.stz(a); 5 }
 
             _ => panic!("Unknown opcode: {:#04X}", opcode),
         }
@@ -113,5 +108,37 @@ impl<B: Bus> Cpu<B> {
     fn lda(&mut self, addr: u16) {
         self.a = self.read(addr);
         self.update_nz(self.a);
+    }
+
+    // Load X with Memory
+    fn ldx(&mut self, addr: u16) {
+        self.x = self.read(addr);
+        self.update_nz(self.x);
+    }
+
+    // Load Y with Memory
+    fn ldy(&mut self, addr: u16) {
+        self.y = self.read(addr);
+        self.update_nz(self.y);
+    }
+
+    // Store Accumulator to Memory
+    fn sta(&mut self, addr: u16) {
+        self.write(addr, self.a);
+    }
+
+    // Store X to Memory
+    fn stx(&mut self, addr: u16) {
+        self.write(addr, self.x);
+    }
+
+    // Store Y to Memory
+    fn sty(&mut self, addr: u16) {
+        self.write(addr, self.y);
+    }
+
+    // Store Zero to Memory (CMOS)
+    fn stz(&mut self, addr: u16) {
+        self.write(addr, 0);
     }
 }
