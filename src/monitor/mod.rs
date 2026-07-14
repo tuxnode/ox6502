04b2c83979e07b48d0556fd90b050bd3eec8b15e
tuxnode <tuxnode@outlook.com>
2026-07-14 18:46:05 +0800

    feat: add Monitor and TraceEntry abstractions

--- src/monitor/mod.rs
+++ src/monitor/mod.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashSet, VecDeque};
+
 mod commands;
 mod disass;
 
@@ -5,3 +7,23 @@ use crate::bus::Bus;
 use crate::cpu::Cpu;
 
 pub fn run<B: Bus>(cpu: &mut Cpu<B>) {}
+
+pub struct TraceEntry {
+    pub addr: u16,
+    pub text: String,
+    pub cycles: u8,
+}
+
+pub struct Monitor {
+    breakpoints: HashSet<u16>,
+    trace: VecDeque<TraceEntry>,
+}
+
+impl Monitor {
+    pub fn new(&mut self) -> Self {
+        Self {
+            breakpoints: HashSet::new(),
+            trace: VecDeque::new(),
+        }
+    }
+}
