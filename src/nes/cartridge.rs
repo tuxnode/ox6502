49d3b23b16e7990546e14092dcf2d4eeb84b9cc6
tuxnode <tuxnode@outlook.com>
2026-07-16 22:58:35 +0800

    docs: add iNES Header Flags Reference

--- src/nes/cartridge.rs
+++ src/nes/cartridge.rs
@@ -4,7 +4,7 @@
  */
 
 /*
-Flags Reference:
+Flags_6 Reference:
 76543210
 ||||||||
 |||||||+- Nametable arrangement: 0: vertical arrangement ("horizontal mirrored") (CIRAM A10 = PPU A11)
@@ -15,7 +15,42 @@ Flags Reference:
 ++++----- Lower nybble of mapper number
 */
 
-pub struct iNESHEADER {
+/*
+* Flag_7 Reference:
+76543210
+||||||||
+|||||||+- VS Unisystem
+||||||+-- PlayChoice-10 (8 KB of Hint Screen data stored after CHR data)
+||||++--- If equal to 2, flags 8-15 are in NES 2.0 format
+++++----- Upper nybble of mapper number
+*/
+
+/*
+* Flag_8:
+76543210
+||||||||
+++++++++- PRG RAM size
+*/
+
+/*
+* Flag_9:
+76543210
+||||||||
+|||||||+- TV system (0: NTSC; 1: PAL)
++++++++-- Reserved, set to zero
+*/
+
+/**
+* Flag_10:
+76543210
+  ||  ||
+  ||  ++- TV system (0: NTSC; 2: PAL; 1/3: dual compatible)
+  |+----- PRG RAM ($6000-$7FFF) (0: present; 1: not present)
+  +------ 0: Board has no bus conflicts; 1: Board has bus conflicts
+*/
+use std::error::Error;
+
+pub struct Cartridge {
     Magic: u32,       // ascii "NES" with MS-DOS end of file
     prg_rom_size: u8, // Size of PRG ROM in 16 KB units
     chr_rom_size: u8, //Size of CHR ROM in 8 KB units (value 0 means the board uses CHR RAM)
@@ -26,3 +61,5 @@ pub struct iNESHEADER {
     flags_10: u8,
     padding: [u8; 5],
 }
+
+pub fn parse(data: &[u8]) -> Result<Cartridge, Error> {}
