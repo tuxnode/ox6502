c316f423c597e5a6cd93619390b3ae8f950fefab
tuxnode <tuxnode@outlook.com>
2026-07-20 09:24:34 +0800

    feat: implement basic pulse

---
+++ src/nes/apu/mod.rs
@@ -0,0 +1,19 @@
+/**
+ * APU References: https://www.nesdev.org/wiki/APU_basics
+ */
+mod pulse;
+
+use pulse::Pulse;
+
+pub struct Apu {
+    pulse1: Pulse,
+    pulse2: Pulse,
+    // triangle: Triangle,
+    // noise: Noise,
+    // dmc: Dmc,
+    // frame_counter_mode: u8,
+    // frame_step: u8,
+    // frame_counter: u32,
+    // sample_divider: u32,
+    // sample_buffer: Vec<f32>,
+}
--- src/nes/mod.rs
+++ src/nes/mod.rs
@@ -1,3 +1,4 @@
+pub mod apu;
 pub mod cartridge;
 pub mod joypad;
 pub mod mapper;
